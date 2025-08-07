3894382835a2067b6eba3e0c59071b37bba7ff0a
corpacademia <72709902+corpacademia@users.noreply.github.com>
2025-08-07 22:21:07 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 
 // CORS config
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://app.golabing.ai/",
   credentials: true,
   methods: "GET, POST, PUT, DELETE, OPTIONS",
   allowedHeaders: "Content-Type, Authorization"
